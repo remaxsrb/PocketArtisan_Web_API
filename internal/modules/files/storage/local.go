784b830d2521a5e237ad8832b4f895e1cba1fed2
Marko <remaxsrb@protonmail.com>
2026-06-06 19:43:09 +0200

    refactored file service to sort out incoming files

--- internal/modules/files/storage/local.go
+++ internal/modules/files/storage/local.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"io"
+	"mime/multipart"
 	"os"
 	"path/filepath"
 )
@@ -14,12 +16,27 @@ func NewLocalStorage(basePath, baseURL string) *LocalStorage {
 	return &LocalStorage{BasePath: basePath, BaseURL: baseURL}
 }
 
-func (l *LocalStorage) SaveFile(fileName string, data []byte) (string, error) {
-	path := filepath.Join(l.BasePath, fileName)
-	if err := os.WriteFile(path, data, 0644); err != nil {
+func (l *LocalStorage) SaveFile(file *multipart.FileHeader, purpose string) (string, error) {
+
+	src, _ := file.Open()
+	defer src.Close()
+	content, _ := io.ReadAll(src)
+
+	//head := make([]byte, 8192)
+	//if len(content) > 8192 {
+	//	head = content[:8192]
+	//} else {
+	//	head = content
+	//}
+
+	fileName := file.Filename
+	path := filepath.Join(l.BasePath, purpose, fileName)
+	os.MkdirAll(filepath.Dir(path), 0755)
+
+	if err := os.WriteFile(path, content, 0644); err != nil {
 		return "", err
 	}
-	return l.BaseURL + "/" + fileName, nil
+	return l.BaseURL + "/" + purpose + "/" + fileName, nil
 }
 
 func (l *LocalStorage) DeleteFile(fileName string) error {
--- internal/modules/files/upload/controller.go
+++ internal/modules/files/upload/controller.go
@@ -1,10 +1,10 @@
 package upload
 
 import (
-	"io"
 	"net/http"
 
 	"PocketArtisan/internal/modules/files/storage"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,16 +13,12 @@ func RegisterRoutes(router *gin.RouterGroup, s storage.Storage) {
 
 	router.POST("/upload", func(c *gin.Context) {
 		file, err := c.FormFile("file")
+		purpose := c.PostForm("purpose")
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "file required"})
 			return
 		}
-
-		src, _ := file.Open()
-		defer src.Close()
-		buf, _ := io.ReadAll(src)
-
-		url, err := uc.Execute(file.Filename, buf)
+		url, err := uc.Execute(file, purpose)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
