9625125b8326be862b82f43b33c469a9a7bfd26c
Marko <remaxsrb@protonmail.com>
2026-06-06 17:35:54 +0200

    modified dto for setting profile picture snake - camel case change

--- internal/modules/users/common/set_profile_picture/dto.go
+++ internal/modules/users/common/set_profile_picture/dto.go
@@ -2,5 +2,5 @@ package set_profile_picture
 
 type SetProfilePictureRequest struct {
 	Username          string `json:"username" binding:"required"`
-	NewProfilePicture string `json:"new_profile_picture" binding:"required"`
+	NewProfilePicture string `json:"newProfilePicture" binding:"required"`
 }
