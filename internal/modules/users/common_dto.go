ac5df8d81a048adc3db336384f4f048669ee2ce6
Marko <remaxsrb@protonmail.com>
2026-06-08 20:44:42 +0200

    refactored login response based on role

--- internal/modules/users/common_dto.go
+++ internal/modules/users/common_dto.go
@@ -1,6 +1,7 @@
 package users
 
 type CraftsmanResponse struct {
+	ID              int64   `json:"id" ommitempty:"id"`
 	Firstname       string  `json:"firstname"`
 	Lastname        string  `json:"lastname"`
 	Username        string  `json:"username" gorm:"column:username"`
@@ -10,13 +11,16 @@ type CraftsmanResponse struct {
 	Craft           string  `json:"craft"`
 	Rating          float64 `json:"rating"`
 	NumberOfRatings int     `json:"numberRatings"`
+	Role            string  `json:"role" ommitempty:"role"`
 }
 
 type RegularUserResponse struct {
+	ID             int64  `json:"id" ommitempty:"id"`
 	Firstname      string `json:"firstname"`
 	Lastname       string `json:"lastname"`
 	Username       string `json:"username" gorm:"column:username"`
 	Email          string `json:"email"`
 	ProfilePicture string `json:"profilePicture"`
 	Gender         string `json:"gender"`
+	Role           string `json:"role" ommitempty:"role"`
 }
