398049aa3a31cf74c5d40e55d0f18ba8eb78bfb2
remax <remaxsrb@protonmail.com>
2026-07-03 13:33:40 +0200

    Updated cors policy to allow requests from napravimi.com and www.napravimi.com, in addition to localhost:4200 for development. Also refactored the default avatar URL generation to use the R2_PUBLIC_URL environment variable for deployment, falling back to local assets for development.

--- internal/http/routes/cors.go
+++ internal/http/routes/cors.go
@@ -7,7 +7,11 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowOrigins: []string{
+			"http://localhost:4200",
+			"https://napravimi.com",
+			"https://www.napravimi.com",
+		},
 		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "OPTIONS", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
